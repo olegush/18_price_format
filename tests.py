bb82344a76fd39a08bceeda3e8cae68fb77aa91b
olegush <43958767+olegush@users.noreply.github.com>
2018-11-23 13:34:28 +0300

    ad

--- tests.py
+++ tests.py
@@ -3,25 +3,38 @@ from format_price import format_price
 
 
 class FomatPriceTest(unittest.TestCase):
+    def test_int_price(self):
+        price = format_price('1250000')
+        self.assertEqual(price, '1 250 000.00')
+
     def test_float_price(self):
         price = format_price('2354.0000')
-        self.assertEqual(price, '2 354')
-
-    def test_rub_kop_price(self):
-        price = format_price('5436руб.80коп.')
-        self.assertEqual(price, '5 437')
+        self.assertEqual(price, '2 354.00')
 
     def test_comma_price(self):
         price = format_price('3657,356')
-        self.assertEqual(price, '3 657')
+        self.assertEqual(price, '3 657.36')
 
     def test_correct_price(self):
         price = format_price('3 657')
-        self.assertEqual(price, '3 657')
+        self.assertEqual(price, '3 657.00')
 
-    def test_float__rub_price(self):
-        price = format_price(' 8736.000 руб.')
-        self.assertEqual(price, '8 736')
+    def test_float__kop(self):
+        price = format_price('0.1')
+        self.assertEqual(price, '0.10')
+
+    def test_float__kop_nozero(self):
+        price = format_price('.1')
+        self.assertEqual(price, '0.10')
+
+    def test_backspaced_price(self):
+        price = format_price(' 3657 ')
+        self.assertEqual(price, '3 657.00')
+
+    def test_incorrect_price(self):
+        self.assertIsNone(format_price('817t36.5634'))
+        self.assertIsNone(format_price('8.3656.34'))
+        self.assertIsNone(format_price('3-36'))
 
 
 if __name__ == '__main__':
