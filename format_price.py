068f276acc5b6db8c6bd015e307246fa45809657
olegush <43958767+olegush@users.noreply.github.com>
2018-11-23 13:33:13 +0300

    improved the code

--- format_price.py
+++ format_price.py
@@ -2,32 +2,15 @@ import sys
 
 
 def format_price(price):
-    price_formatted = price.replace(' ', '')
-    if not price_formatted.isdigit():
-        price_formatted = formaf_price_with_separator(price)
-    if int(price_formatted) > 999:
-        price_formatted = '{} {}'.format(
-            str(price_formatted)[:-3],
-            str(price_formatted)[-3:]
-        )
-    return price_formatted
-
-
-def formaf_price_with_separator(price):
-    price_kop = ''
-    flag = False
-    index_kop = len(price)
-    for char in price[::-1]:
-        if char.isdigit() and not flag:
-            price_kop += char
-        elif price_kop:
-            flag = True
-            index_kop -= 1
-    price_rub = ''
-    for char in price[:-index_kop]:
-        if char.isdigit():
-            price_rub += char
-    return round(float(price_rub + '.' + price_kop[::-1]))
+    try:
+        preformatted_price = price.replace(' ', '').replace(',', '.')
+        floated_price = float(str(preformatted_price))
+    except ValueError:
+        pass
+    except TypeError:
+        pass
+    else:
+        return '{:,.2f}'.format(floated_price).replace(',', ' ')
 
 
 if __name__ == '__main__':
