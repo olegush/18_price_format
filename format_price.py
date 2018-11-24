877a376b661dff7406caa02aee14c719acd16a84
olegush <43958767+olegush@users.noreply.github.com>
2018-11-24 08:28:48 +0300

    fixed bugs

--- format_price.py
+++ format_price.py
@@ -3,21 +3,19 @@ import sys
 
 def format_price(price):
     try:
-        preformatted_price = price.replace(' ', '').replace(',', '.')
-        floated_price = float(str(preformatted_price))
-    except ValueError:
-        pass
-    except TypeError:
+        floated_price = float(price.replace(' ', ''))
+    except (ValueError, TypeError):
         pass
     else:
-        return '{:,.2f}'.format(floated_price).replace(',', ' ')
+        preformatted_price = '{:,.2f}'.format(floated_price)
+        return preformatted_price.replace(',', ' ').replace('.00', '')
 
 
 if __name__ == '__main__':
     try:
         user_price = sys.argv[1]
     except IndexError:
-        exit('Please use script parameter (any formatted price)')
+        exit('Please use script parameter (price)')
     else:
         print('Formatted price:')
         print(format_price(user_price))
