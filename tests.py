f8ee2fc7eab6393c97bea279b608bb2acb99e1c4
olegush <43958767+olegush@users.noreply.github.com>
2018-11-25 09:21:50 +0300

    added more tests

--- tests.py
+++ tests.py
@@ -27,11 +27,31 @@ class FomatPriceTest(unittest.TestCase):
         price = format_price(' 3657.578 ')
         self.assertEqual(price, '3 657.58')
 
-    def test_incorrect_price(self):
+    def test_price_with_letters(self):
         self.assertIsNone(format_price('817t36.5634'))
+        self.assertIsNone(format_price(' zfg# 817t36.5634 '))
+        self.assertIsNone(format_price('price'))
+
+    def test_price_with_points(self):
         self.assertIsNone(format_price('8.3656.34'))
+        self.assertIsNone(format_price('2.338.3656.34'))
+
+    def test_price_with_special_chars(self):
         self.assertIsNone(format_price('3-36'))
+        self.assertIsNone(format_price('3?36'))
+        self.assertIsNone(format_price(' 3+36 '))
+        self.assertIsNone(format_price('"3-36"'))
+        self.assertIsNone(format_price('3#36#'))
+
+    def test_price_with_comma(self):
         self.assertIsNone(format_price('3657,356'))
+        self.assertIsNone(format_price('3,657.356'))
+
+    def test_incorrect_type(self):
+        self.assertIsNone(format_price(list()))
+        self.assertIsNone(format_price(dict()))
+        self.assertIsNone(format_price(tuple()))
+        self.assertIsNone(format_price(set()))
 
 
 if __name__ == '__main__':
